14a2df5eb91180f572323a7bf9b8779f4e1b2dd1
shaikhrahil <smohammadrahil@gmail.com>
2021-07-14 22:08:16 +0530

    🔨 fix(accounts): changed dtos from Pascal case to camelcase for user model

--- accounts/model.go
+++ accounts/model.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
-	Name     string `validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
-	Email    string `gorm:"uniqueIndex;type:varchar(255)" validate:"required,email,min=3,max=32" partial_validate:"omitempty,email,min=3,max=32"`
-	Username string `gorm:"uniqueIndex;type:varchar(255)" validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"deleteddAt" gorm:"index"`
+
+	FirstName string `json:"firstName" validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
+	LastName  string `json:"lastName" validate:"omitempty,min=3,max=32"`
+	Email     string `json:"email" gorm:"uniqueIndex;type:varchar(255)" validate:"required,email,min=3,max=32" partial_validate:"omitempty,email,min=3,max=32"`
+	UserName  string `json:"userName" gorm:"uniqueIndex;type:varchar(255)" validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
 }
