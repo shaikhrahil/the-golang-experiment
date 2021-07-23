39cddc301d63dbf39e4e1c422bba3fa0a773eaa2
shaikhrahil <smohammadrahil@gmail.com>
2021-07-23 22:47:47 +0530

    🔨 fix: first/lastname(s) were not added to gorm Model

--- accounts/lib/model.go
+++ accounts/lib/model.go
@@ -13,8 +13,8 @@ type User struct {
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `json:"deleteddAt" gorm:"index"`
 
-	FirstName string `json:"firstName" validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
-	LastName  string `json:"lastName" validate:"omitempty,min=3,max=32"`
+	FirstName string `json:"firstName" gorm:"type:varchar(255)" validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
+	LastName  string `json:"lastName" gorm:"type:varchar(255)" validate:"omitempty,min=3,max=32"`
 	Email     string `json:"email" gorm:"uniqueIndex;type:varchar(255)" validate:"required,email,min=3,max=32" partial_validate:"omitempty,email,min=3,max=32"`
 	UserName  string `json:"userName" gorm:"uniqueIndex;type:varchar(255)" validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
 	Password  string `json:"--" gorm:"uniqueIndex;type:varchar(255)" validate:"required,min=10,max=32" partial_validate:"omitempty,min=10,max=32"`
