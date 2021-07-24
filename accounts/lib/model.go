04e37e26bcbbbe40c51af850c04b9ae834914623
shaikhrahil <smohammadrahil@gmail.com>
2021-07-24 19:32:16 +0530

    🔨 fix: password was exposed in apis

--- accounts/lib/model.go
+++ accounts/lib/model.go
@@ -17,7 +17,7 @@ type User struct {
 	LastName  string `json:"lastName" gorm:"type:varchar(255)" validate:"omitempty,min=3,max=32"`
 	Email     string `json:"email" gorm:"uniqueIndex;type:varchar(255)" validate:"required,email,min=3,max=32" partial_validate:"omitempty,email,min=3,max=32"`
 	UserName  string `json:"userName" gorm:"uniqueIndex;type:varchar(255)" validate:"required,min=3,max=32" partial_validate:"omitempty,min=3,max=32"`
-	Password  string `json:"--" gorm:"uniqueIndex;type:varchar(255)" validate:"required,min=10,max=32" partial_validate:"omitempty,min=10,max=32"`
+	Password  string `json:"-" gorm:"uniqueIndex;type:varchar(255)" validate:"required,min=10,max=32" partial_validate:"omitempty,min=10,max=32"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
