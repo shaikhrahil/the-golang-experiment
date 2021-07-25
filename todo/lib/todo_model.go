0deb96e2ffd8269ec3e58270e3e90bedf7554dc0
shaikhrahil <smohammadrahil@gmail.com>
2021-07-25 20:20:23 +0530

    ♻️ refactor: using <pkg>_<filetype>.go filename pattern for convenience

--- todo/lib/todo_model.go
+++ todo/lib/todo_model.go
@@ -12,6 +12,7 @@ type Todo struct {
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `json:"deleteddAt" gorm:"index"`
 
-	Title   string `json:"title" validate:"omitempty,min=3,max=32" gorm:"type:varchar(255)"`
-	Content string `json:"content" validate:"omitempty,min=3,max=32" gorm:"type:varchar(255)"`
+	Title   string `json:"title" gorm:"type:varchar(255)" validate:"omitempty,min=3,max=32"`
+	Content string `json:"content" gorm:"type:varchar(255)" validate:"omitempty,min=3,max=32"`
+	Done    bool   `json:"done" gorm:"type:boolean,default:false"`
 }
